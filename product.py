0029270f2e5f6c66deb7f9e66fe5348651207bc2
Tanvir <tanvirsiddiq96@gmail.com>
2025-02-28 06:49:12 +0000

    itertools.permutation

--- product.py
+++ product.py
@@ -1,22 +1,7 @@
-def arrange_data(student_number):
-    scores = {}
-    
-    for _ in range(student_number):
-        student_id = input("student id:")
-        score = int(input("score:"))
-        scores[student_id] = score
-    
-    sorted_scores = sorted(scores.items())
-
-    for student, score in sorted_scores:
-
-        print(f"Student ID: {student}, Score: {score}")
-        if score > 59:
-            print(f"student id {student} is pass")
-        else:
-            print(f"student id {student} is fail")
-
-if __name__ == "__main__":
-    student_number = int(input("student number:"))
-    arrange_data(student_number)
-
+from itertools import permutations
+string, inte = (input().split())
+n = int(inte)
+sorted_string = sorted(string)
+for x in permutations(sorted_string,n) :
+    print("".join(x))
+   
