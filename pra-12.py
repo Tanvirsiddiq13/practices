23ddd17d6212bb104148f0e125f2da2531618a83
Tanvir <tanvirsiddiq96@gmail.com>
2025-03-02 08:18:22 +0000

    find the angle

--- pra-12.py
+++ pra-12.py
@@ -1,6 +1,5 @@
-from cmath import phase
-user_input = complex(input().strip())
-a = user_input.real
-b = user_input.imag
-print(phase(complex(a,b)))
-print(abs(complex(a,b)))
+import math
+AB = float(input())
+BC = float(input())
+angle = math.degrees(math.atan2(AB,BC))
+print(f"{round(angle)}\u00B0")
